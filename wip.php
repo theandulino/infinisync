11b00e97e418f648158d151a7d159715a5441977
theandulino <105876908+theandulino@users.noreply.github.com>
2025-05-26 15:36:21 +0200

    infinisync

---
+++ wip.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'data/header.php';
+?>
+<div class="container">
+<h1>Work in Progress</h1>
+<p>
+    This page is still under construction. Please check back later.
+</p>
+    </div>
